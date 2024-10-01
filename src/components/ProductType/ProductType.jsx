c243913834e0bf1f8a741ce82a6348c7586b7c02
nicolasruoco <nicolas.ruoco@germinare.org.br>
2024-10-01 09:30:34 -0300

    adding the style to the categorie component

--- src/components/ProductType/ProductType.jsx
+++ src/components/ProductType/ProductType.jsx
@@ -3,8 +3,8 @@ import styles from './ProductType.module.css'
 export default function ProductType({ text, image }) {
     return (
         <div className={styles.container}>
-            <img src={image} />
-            <p>{text}</p>
+            <img src={image} className={styles.image}/>
+            <p classname={styles.text}>{text}</p>
         </div>
     )
 }
